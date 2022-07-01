5c767ea89abfbb087b8c1a43e85af3f20de16640
skibort <kibort.stanislaw@gmail.com>
2022-07-01 14:27:25 +0200

    game repository impl

--- TournamentLadder.Infrastructure/Repositories/Game/GameRepository.cs
+++ TournamentLadder.Infrastructure/Repositories/Game/GameRepository.cs
@@ -1,31 +1,67 @@
+using Microsoft.EntityFrameworkCore;
+using TournamentLadder.Infrastructure.Context;
 using TournamentLadder.Infrastructure.Entities;
+using TournamentLadder.Infrastructure.Exceptions;
 
 namespace TournamentLadder.Infrastructure.Repositories;
 
 public class GameRepository : IGameRepository
 {
-    public Task<IEnumerable<Game>> GetAll()
+    private readonly MainContext _mainContext;
+
+    public GameRepository(MainContext mainContext)
     {
-        throw new NotImplementedException();
+        _mainContext = mainContext;
     }
 
-    public Task<Game> GetById(int id)
+    public async Task<IEnumerable<Game>> GetAll()
     {
-        throw new NotImplementedException();
+        return await _mainContext.Game.ToListAsync();
     }
 
-    public Task Add(Game entity)
+    public async Task<Game> GetById(int id)
     {
-        throw new NotImplementedException();
+        var game = await _mainContext.Game.SingleOrDefaultAsync(x => x.Id == id);
+        if (game != null)
+        {
+            return game;
+        }
+
+        throw new EntityNotFoundException();
     }
 
-    public Task Update(Game entity)
+    public async Task Add(Game entity)
     {
-        throw new NotImplementedException();
+        entity.DateOfCreation = DateTime.UtcNow;
+        await _mainContext.AddAsync(entity);
+        await _mainContext.SaveChangesAsync();
     }
 
-    public Task DeleteById(int id)
+    public async Task Update(Game entity)
     {
-        throw new NotImplementedException();
+        var gameToUpdate = await _mainContext.Game.SingleOrDefaultAsync(x => x.Id == entity.Id);
+        if (gameToUpdate != null)
+        {
+            gameToUpdate.TeamScores = entity.TeamScores;
+            await _mainContext.SaveChangesAsync();
+        }
+        else
+        {
+            throw new EntityNotFoundException();
+        }
+    }
+
+    public async Task DeleteById(int id)
+    {
+        var gameToDelete = await _mainContext.Game.SingleOrDefaultAsync(x => x.Id == id);
+        if (gameToDelete != null)
+        {
+            _mainContext.Game.Remove(gameToDelete);
+            await _mainContext.SaveChangesAsync();
+        }
+        else
+        {
+            throw new EntityNotFoundException();
+        }
     }
 }
